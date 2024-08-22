ded8dd4fc726b38c4fb2a52d33472255ff574dd7
0xSufi <0xsufi@noma.money>
2024-08-22 17:22:24 -0400

    CTA banner

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,4 +1,4 @@
-import { Flex, Stat, StatLabel, StatNumber, chakra } from '@chakra-ui/react';
+import { Box, Button, Flex, Text, Stat, StatLabel, StatNumber, chakra } from '@chakra-ui/react';
 import { isMobile } from "react-device-detect";
   
   interface StatsCardProps {
@@ -28,14 +28,51 @@ import { isMobile } from "react-device-detect";
   export default function Stats() {
     return (
         <>
-          <Flex>
-            <chakra.h2
-              textAlign={'center'}
-              py={10}
-              mx={'auto'}>
-              Protocol design<br/> 
-              that transcends limitations
-            </chakra.h2>
+          <Box
+            w="100vw"
+            h="100vh"
+            bg="gray.800"
+            color="white"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            textAlign="center"
+            position={"absolute"}
+            left={0}
+            right={0}
+            height={isMobile? 480 : 300}
+            p={4}
+            backgroundColor={"#161616"}
+            backgroundSize={"100%"}
+          >
+          <Flex direction="column" align="center" justify="center" w="full">
+              <chakra.h2 fontSize={isMobile ? "2xl" : "4xl"} mt={isMobile ? 120 : 60}>
+                Enroll in the <Text color={'lightgreen'} as={'span'}>bootstrap</Text> event
+              </chakra.h2>
+              <Text fontSize={isMobile ? "small" : "medium"} w={isMobile ? "90%" : "60%"} align={"left"}>
+                A presale will be conducted in order to bootstrap the floor liquidity and fund initial operations. Details of the operation will be communicated over our social network and media channels including this web page.
+              </Text>
+              <Flex
+                direction={isMobile ? "column" : "row"}
+                gap={4}
+                w={isMobile ? "full" : "auto"}
+              >
+                <a href="https://discord.gg/nomaprotocol" target="_blank">
+                <Button p={15} minW={100} fontWeight={600} background={"black"} color={"lightgreen"} mb={isMobile ? 120 : 60} mt={20}>
+                    Get whitelisted
+                </Button>
+                </a>
+              </Flex>
+            </Flex>
+          </Box>
+          <Flex mt={isMobile ? "125%" : "25%"}>
+              <chakra.h2
+                textAlign={'center'}
+                py={10}
+                mx={'auto'}>
+                Protocol design<br/> 
+                that transcends limitations
+              </chakra.h2>
           </Flex>
           <Flex 
             direction={isMobile? "column" : "row"}
