8702eaa7bf92dca590700460a01c3ddd1e60cf6b
0xsufi <0xsufi@noma.money>
2024-11-12 14:00:15 -0400

    fix typo

--- src/pages/Bootstrap.tsx
+++ src/pages/Bootstrap.tsx
@@ -147,7 +147,7 @@ const Bootstrap: React.FC = () => {
             </Box>
             <Box>
               <Text fontSize="md" color="gray.500">
-                Join the whitelist to participate in the event and earn $NOMA points. Seates are limited so secure your spot now!
+                Join the whitelist to participate in the event and earn $NOMA points. Seats are limited so secure your spot now!
               </Text>
             </Box>
           </SimpleGrid>
