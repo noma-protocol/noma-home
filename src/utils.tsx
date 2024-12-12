34aff7e965afbdf2a74d7c8568daf59cc971c60e
0xsufi <0xsufi@noma.money>
2024-12-12 13:43:03 -0400

    helper function

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,5 +1,4 @@
 
-
 export const commify = (number) => {
   if (number === undefined) return "";
     let numStr = number.toString();
@@ -7,4 +6,15 @@ export const commify = (number) => {
     integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return decimalPart ? `${integerPart}.${decimalPart}` : integerPart;
   }
-  
+
+
+// Generate a bytes32 string
+export function generateBytes32String(text) {
+  if (text.length > 32) {
+      throw new Error("String exceeds 32 bytes");
+  }
+
+  // Convert the string to a UTF-8 encoded bytes array and pad it to 32 bytes
+  const hex = Buffer.from(text, "utf8").toString("hex");
+  return "0x" + hex.padEnd(64, "0"); // Ensure it's padded to 64 hex characters (32 bytes)
+}
